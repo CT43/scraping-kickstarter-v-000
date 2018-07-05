e2a2f03f454474a157e34be469dd62e196b3e5e7
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-05 21:23:40 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,8 @@
 # require libraries/modules here
 require 'nokogiri'
 require 'pry'
+# projects: kickstarter.css("li.project.grid_4")
+# project.css("h2.bbcard_name strong a").text
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
